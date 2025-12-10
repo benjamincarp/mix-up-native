a0c2117cd3af53cefb8516e579a43e3454f551c9
benjamincarp <benjamincarp@gmail.com>
2025-12-09 22:35:05 -0600

    title styling

--- app/index.tsx
+++ app/index.tsx
@@ -4,12 +4,43 @@ export default function Index() {
   return (
     <View
       style={{
+        backgroundColor: "#1a231a",
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View 
+        style={{
+          margin: 5,
+          marginBottom: 10,
+          paddingLeft: 5,
+          paddingRight: 5,
+          borderStyle: "solid",
+          borderColor: "#dedcdc",
+          borderWidth: 2
+        }}>
+        <View
+          style={{
+            padding: 20, 
+            borderStyle: "solid",
+            marginTop: -10, 
+            marginBottom: -10, 
+            borderColor: "#dedcdc",
+            borderWidth: 2
+          }}>
+          <Text
+            style={{ 
+              color: "#dedcdc", 
+              fontSize: 30, 
+              fontWeight: "bold" 
+            }}
+          >
+            Section Title
+        </Text>
+        </View>
+      </View>
+      
     </View>
   );
 }
