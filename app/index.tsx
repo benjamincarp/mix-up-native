a214e07b2c7687d7f3e477e6c3b6c264a38524d1
benjamincarp <benjamincarp@gmail.com>
2025-12-10 21:12:43 -0600

    use map for link list

--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,25 @@ import MainContainer from "@/components/main_container";
 import { Link } from "expo-router";
 
 export default function Index() {
+  const drinks = ["Manhattan", "Old Fashioned"];
+
+  const linkList = drinks.map((drink) => (
+    <Link
+      key={drink}
+      href={`/drinks/${drink}`} 
+      style={{
+        color: "#dedcdc", 
+        fontSize: 15,
+        padding: 10, 
+      }}>
+        {drink}
+    </Link>
+  ));
+  
   return (
     <MainContainer>
       <Header text="Welcome to Mix Up Native!" />
-      <Link href="/drinks/manhattan" style={{color: "#dedcdc", 
-              fontSize: 15 }}>Manhattan</Link>
-      <Link href="/drinks/old fashioned"  style={{color: "#dedcdc", 
-              fontSize: 15 }}>Old Fashioned</Link>
+      {linkList}
     </MainContainer>
   );
 }
